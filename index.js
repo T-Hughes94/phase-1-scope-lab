119a2085ce0a60e6e00e32848344949f86c12cf9
Terence Hughes <Terencehughes450@gmail.com>
2023-08-02 09:38:25 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+var customerName = 'bob';// Write your solution in this file!
+const leastFavoriteCustomer = 'Terence'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob';
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Terence';
+}
